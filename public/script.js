069ea32b64c0ce9495b6241f117dec4dfec17e20
vinnietito <vincentnjuguna69@gmail.com>
2025-06-04 12:46:46 +0300

    Commits

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const baseURL = 'http://localhost:3000';
+
+async function logEntry() {
+    const plate = document.getElementById('plate').value.toUpperCase();
+    if (!plate) return alert('Enter plate number');
+    await fetch(`${baseURL}/entry`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ plate })
+    });
+    refreshTables();
+}
