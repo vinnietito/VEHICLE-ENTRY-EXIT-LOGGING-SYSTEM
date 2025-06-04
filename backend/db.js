42761f3376ab3abb232c98a5731370a1dac4475a
vinnietito <vincentnjuguna69@gmail.com>
2025-06-04 11:50:10 +0300

    Commits

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+
+const connection = mysql.CreateConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'V@38080135k',
+    database: 'vehicle_logs'
+});
