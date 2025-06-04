5ae1c6c980f2b6e7d85e1534f68fa066434fd31e
vinnietito <vincentnjuguna69@gmail.com>
2025-06-04 11:57:58 +0300

    Commits

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,8 @@ const app = express();
 const cors = require('cors');
 const connection = require('./db');
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// Log vehicle entry
